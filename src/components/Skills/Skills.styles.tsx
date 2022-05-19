76113d1e5c98f40ff106b79432e84e00aaf46410
Joshua <olajidejoshua4real@gmail.com>
2022-05-19 14:49:28 +0100

    Add Skills component

---
+++ src/components/Skills/Skills.styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: green;
+  height: 400px;
+`;
+
+export { Container };
---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./Skills.styles";
+
+function Skills() {
+  return (
+    <Container>
+      <h1>List of Styles</h1>
+    </Container>
+  );
+}
+
+export default Skills;
