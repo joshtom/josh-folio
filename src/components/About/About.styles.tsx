c316b07925f3a5b251e9fafb36e187def496e5e8
Joshua <olajidejoshua4real@gmail.com>
2022-06-25 17:23:09 +0100

    Rework about page by adding accordion

--- src/components/About/About.styles.tsx
+++ src/components/About/About.styles.tsx
@@ -32,4 +32,92 @@ const Overflow = styled.div`
   overflow: hidden;
 `;
 
-export { AboutContainer, AboutParagraph, Overflow };
+const Accordion = styled.div`
+  &[aria-expanded="true"] {
+  }
+`;
+
+const AccordionHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: none;
+  position: relative;
+  display: block;
+  width: 100%;
+  border: none;
+  text-align: left;
+  /* padding: 25px 60px 25px 25px; */
+  padding: 25px 0;
+  font-weight: 500;
+  font-size: 17px;
+  font-family: inherit;
+  transition: color 0.2s linear;
+  cursor: pointer;
+  letter-spacing: ${({ theme }) => theme.letterSpacing("0.08")};
+  border-bottom: 1px solid ${cVar("grayMid")};
+
+  &:focus {
+    outline: none;
+  }
+
+  &::after,
+  &::before {
+    content: "";
+    position: absolute;
+    right: 25px;
+    right: 0;
+    top: 50%;
+    width: 40px;
+    height: 2px;
+    background-color: ${cVar("pink")};
+
+    ${({ theme }) => theme.media.mobile} {
+      width: 25px;
+    }
+  }
+
+  &::before {
+    transform: rotate(-90deg);
+    transition: transform 0.35s cubic-bezier(0.65, 0.05, 0.36, 1);
+  }
+
+  &[aria-expanded="true"] {
+    &::before {
+      transform: rotate(0deg);
+    }
+  }
+
+  p {
+    font-size: 5vw;
+    color: ${cVar("pink")};
+    font-family: ${cVar("dinAlternate")};
+    text-transform: uppercase;
+
+    ${({ theme }) => theme.media.mobile} {
+      font-size: 7vw;
+    }
+  }
+`;
+
+const AccordionBody = styled.p`
+  color: ${cVar("pink")};
+  line-height: 41px;
+  font-family: ${cVar("dmSans")};
+  padding-top: 20px;
+  opacity: 1;
+  ${({ theme }) => theme.transition.default};
+
+  &[aria-expanded="false"] {
+    opacity: 0;
+  }
+`;
+
+export {
+  AboutContainer,
+  AboutParagraph,
+  Overflow,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+};
--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -20,3 +20,36 @@ export const SkillsetBottom = [
   "sass",
   "typescript",
 ];
+
+export const Panels: { id: number; label: string; content: string }[] = [
+  {
+    id: 1,
+    label: "ENGINEERING",
+    content:
+      "I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.  I've built products that solve real-life problems ranging from Businesses to companies.",
+  },
+  {
+    id: 2,
+    label: "DESIGN",
+    content:
+      "I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.  I've built products that solve real-life problems ranging from Businesses to companies.",
+  },
+  {
+    id: 3,
+    label: "MENTORSHIP",
+    content:
+      "I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.  I've built products that solve real-life problems ranging from Businesses to companies.",
+  },
+  {
+    id: 4,
+    label: "WRITINGS",
+    content:
+      "I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.  I've built products that solve real-life problems ranging from Businesses to companies.",
+  },
+  {
+    id: 5,
+    label: "COMMUNITY",
+    content:
+      "I've built products that solve real-life problems ranging from Businesses to companies with focus on creating fast, and accessible user experiences using advanced web technologies.  I've built products that solve real-life problems ranging from Businesses to companies.",
+  },
+];
